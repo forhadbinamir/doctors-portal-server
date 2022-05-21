561dc5eca45fd650496dfbf9c908bd288113c222
forhadbinamir <forhadbinamir@gmail.com>
2022-05-22 00:14:07 +0300

    added a payment method API

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { send } = require('express/lib/response');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 
 app.use(cors())
 app.use(express.json())
@@ -36,11 +36,68 @@ async function run() {
         const databaseCollection = client.db("doctor_portal").collection("service");
         const bookingCollection = client.db("doctor_portal").collection("booking");
         const userCollection = client.db("doctor_portal").collection("users");
+        const doctorsCollection = client.db("doctor_portal").collection("doctor");
 
-        app.get('/user', async (req, res) => {
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+
+            const service = req.body
+            const price = service.price
+            const amount = price * 100
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+        }
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorsCollection.find().toArray()
+            res.send(doctors)
+        })
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const query = req.body
+            const result = await doctorsCollection.insertOne(query)
+            res.send(result)
+        })
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await doctorsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.get('/user', verifyJWT, async (req, res) => {
             const query = {}
             const cursor = userCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: 'admin' }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
@@ -65,6 +122,13 @@ async function run() {
             }
             const result = await bookingCollection.insertOne(booking)
             return res.send({ success: true, result })
+        })
+
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await bookingCollection.findOne(query)
+            res.send(result)
         })
 
         app.get('/available', async (req, res) => {
@@ -107,7 +171,7 @@ async function run() {
 
         app.get('/service', async (req, res) => {
             const query = {}
-            const cursor = databaseCollection.find(query)
+            const cursor = databaseCollection.find(query).project({ name: 1 })
             const result = await cursor.toArray()
             res.send(result)
         })
